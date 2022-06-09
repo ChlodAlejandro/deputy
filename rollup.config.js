0e8673fd9e2504b0146790d1b4ed3cc40660a9a1
ChlodAlejandro <chlod@chlod.net>
2022-06-09 22:22:36 +0800

    fix banner

--- rollup.config.js
+++ rollup.config.js
@@ -4,12 +4,33 @@ import license from 'rollup-plugin-node-license';
 import * as fs from 'fs';
 import * as path from 'path';
 
+/**
+ *
+ * @param string
+ */
+function blockCommentIfy( string ) {
+	let final = '';
+	const lines = string.toString().split( '\n' );
+	for ( const lineNo in lines ) {
+		const line = lines[ lineNo ];
+
+		if ( +lineNo === 0 ) {
+			final += `/*!\n * ${line}\n`;
+		} else if ( +lineNo === lines.length - 1 ) {
+			final += ` * ${line}\n */`;
+		} else {
+			final += ` * ${line}\n`;
+		}
+	}
+	return final;
+}
+
 export default {
 	input: 'src/deputy.core.ts',
 	output: {
 		dir: 'build',
 		format: 'iife',
-		banner: fs.readFileSync( path.join( __dirname, 'BANNER.txt' ) )
+		banner: blockCommentIfy( fs.readFileSync( path.join( __dirname, 'BANNER.txt' ) ) )
 	},
 	plugins: [ typescript(), nodeResolve( {
 		browser: true
