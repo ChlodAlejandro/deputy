1541e24d2cf74c22818edf600966665d17e8f930
ChlodAlejandro <chlod@chlod.net>
2022-07-07 10:21:29 +0800

    set ts-jest compilation version to ES2022

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
 	},
 	globals: {
 		'ts-jest': {
-			tsconfig: '<rootDir>/tsconfig.json'
+			tsconfig: '<rootDir>/tsconfig.tests.json'
 		}
 	},
 	setupFiles: [ './tests/setup.ts' ]
