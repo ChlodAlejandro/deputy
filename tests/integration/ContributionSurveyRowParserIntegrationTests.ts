c4881e16e7c01d0cecd3ac7f646f5155f7557163
Chlod Alejandro <chlod@chlod.net>
2022-10-18 23:11:23 +0800

    decrease test count for CSR live parse tests

--- tests/integration/ContributionSurveyRowParserIntegrationTests.ts
+++ tests/integration/ContributionSurveyRowParserIntegrationTests.ts
@@ -6,51 +6,13 @@ describe( 'ContributionSurveyRowParser integration tests', () => {
 	const cases: string[] = [
 		'ItsLassieTime',
 		'Dawkeye',
-		'GVnayR',
-		'Marylandstater',
-		'Borsoka',
-		'Wikiwatcher1',
-		'Judgesurreal777',
-		'IWannaABillionaire',
-		'Hantsheroes',
-		'Arrwiki',
-		'FreshCorp619',
-		'Aetheling1125',
-		'Dante8',
-		'Trident13',
-		'Skoojal',
-		'DocOfSoc',
-		'BiggestSataniaFangirl89',
-		'20150927',
-		'Aldebaran69',
-		'Helena Bx',
-		'20180325',
-		'Edelmand',
-		'20190125',
-		'20190724',
-		'20200212',
-		'ZarhanFastfire',
-		'Kiraroshi1976',
-		'Greenock125',
-		'JoeScarce',
-		'20210127',
-		'Manannan51',
-		'20210315',
-		'Amshpatten',
-		'WikiProject Tropical cyclones',
-		'Bluecountrymutt',
-		'Werldwayd',
-		'Shadowwarrior8',
-		'20220220b',
-		'I64s',
-		'20220731',
-		'20221001'
+		'Shadowwarrior8'
 	];
 
 	describe( 'Live parsing tests on open cases', () => {
 
 		for ( const caseName of cases ) {
-			jest.setTimeout( 40e3 );
+			jest.setTimeout( 300e3 );
 			test( 'enwiki:' + caseName, async () => {
 				const caseTitle = 'Wikipedia:Contributor copyright investigations/' + caseName;
 
