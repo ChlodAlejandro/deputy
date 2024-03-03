742e90c93cedb3af22308130d48e247aa852c97c
Chlod Alejandro <chlod@chlod.net>
2024-03-03 14:33:34 +0800

    mw: fix section detection (again)

--- src/wiki/util/sectionHeadingName.ts
+++ src/wiki/util/sectionHeadingName.ts
@@ -4,8 +4,15 @@
  */
 export default function sectionHeadingName( element: HTMLHeadingElement ): string {
 	try {
-		return element.querySelector<HTMLElement>( '.mw-headline' )?.innerText ??
-			element.innerText;
+		// Get only the direct text of .mw-headline
+		// Why? Because DiscussionTools inserts a [subscribe] link in the .mw-headline
+		// element, which we don't want to include in the section name.
+		const headlineElement = element.querySelector<HTMLElement>( '.mw-headline' );
+		const headlineDirectText = Array.from( headlineElement?.childNodes ?? [] )
+			.filter( n => n.nodeType === Node.TEXT_NODE )
+			.reduce( ( acc, n ) => acc + n.textContent, '' )
+			.trim();
+		return headlineDirectText || headlineElement?.innerText;
 	} catch ( e ) {
 		console.error( 'Error getting section name', e, element );
 		throw e;
