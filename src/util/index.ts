dc02cda1eb221851d1237554a6b1bf39a19db84c
Chlod Alejandro <chlod@chlod.net>
2024-04-23 01:03:25 +0800

    util: update indices, gen script

--- src/util/index.ts
+++ src/util/index.ts
@@ -3,18 +3,22 @@ import classMix from './classMix';
 import cleanParams from './cleanParams';
 import cloneRegex from './cloneRegex';
 import copyToClipboard from './copyToClipboard';
+import dangerModeConfirm from './dangerModeConfirm';
 import equalTitle from './equalTitle';
+import findNextSiblingElement from './findNextSiblingElement';
 import fromObjectEntries from './fromObjectEntries';
 import generateId from './generateId';
 import getObjectValues from './getObjectValues';
 import last from './last';
 import matchAll from './matchAll';
 import moveToStart from './moveToStart';
+import organize from './organize';
 import pickSequence from './pickSequence';
 import removeElement from './removeElement';
 import Requester from './Requester';
 import sleep from './sleep';
 import swapElements from './swapElements';
+import unwrapElement from './unwrapElement';
 import unwrapWidget from './unwrapWidget';
 import yesNo from './yesNo';
 export default {
@@ -23,18 +27,22 @@ export default {
 	cleanParams: cleanParams,
 	cloneRegex: cloneRegex,
 	copyToClipboard: copyToClipboard,
+	dangerModeConfirm: dangerModeConfirm,
 	equalTitle: equalTitle,
+	findNextSiblingElement: findNextSiblingElement,
 	fromObjectEntries: fromObjectEntries,
 	generateId: generateId,
 	getObjectValues: getObjectValues,
 	last: last,
 	matchAll: matchAll,
 	moveToStart: moveToStart,
+	organize: organize,
 	pickSequence: pickSequence,
 	removeElement: removeElement,
 	Requester: Requester,
 	sleep: sleep,
 	swapElements: swapElements,
+	unwrapElement: unwrapElement,
 	unwrapWidget: unwrapWidget,
 	yesNo: yesNo
 };
--- src/wiki/util/index.ts
+++ src/wiki/util/index.ts
@@ -1,7 +1,7 @@
-import pagelinkToTitle from './pagelinkToTitle';
 import decorateEditSummary from './decorateEditSummary';
 import delink from './delink';
 import errorToOO from './errorToOO';
+import getApiErrorText from './getApiErrorText';
 import getNativeRange from './getNativeRange';
 import getPageContent from './getPageContent';
 import getPageTitle from './getPageTitle';
@@ -15,6 +15,8 @@ import msgEval from './msgEval';
 import normalizeTitle from './normalizeTitle';
 import nsId from './nsId';
 import openWindow from './openWindow';
+import pagelinkToTitle from './pagelinkToTitle';
+import parseDiffUrl from './parseDiffUrl';
 import performHacks from './performHacks';
 import purge from './purge';
 import renderWikitext from './renderWikitext';
@@ -23,10 +25,10 @@ import sectionHeadingN from './sectionHeadingN';
 import sectionHeadingName from './sectionHeadingName';
 import toRedirectsObject from './toRedirectsObject';
 export default {
-	anchorToTitle: pagelinkToTitle,
 	decorateEditSummary: decorateEditSummary,
 	delink: delink,
 	errorToOO: errorToOO,
+	getApiErrorText: getApiErrorText,
 	getNativeRange: getNativeRange,
 	getPageContent: getPageContent,
 	getPageTitle: getPageTitle,
@@ -40,6 +42,7 @@ export default {
 	normalizeTitle: normalizeTitle,
 	nsId: nsId,
 	openWindow: openWindow,
+	pagelinkToTitle: pagelinkToTitle,
 	performHacks: performHacks,
 	purge: purge,
 	renderWikitext: renderWikitext,
